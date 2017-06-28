0d3cb24785e80f8f03b61544b0e4f3a020819ef6
kalaokay_win <jyq920203@163.com>
2017-06-28 11:25:50 +0800

    commit

--- split.py
+++ split.py
@@ -1,8 +1,18 @@
 import pyperclip
 
+#get value from clipboard
 text=pyperclip.paste()
+
+#separate lines and add stars
 lines = text.split('\n')
 for i in range(len(lines)):
     lines[i] = '*' + lines[i]
 
+#combine list members
+text = '\n'.join(lines)
+
+#pass value to clipboard
 pyperclip.copy(text)
+# from this demo we can learn something like
+# 1.str.join and str.split
+# 2.pyperclip.paste and pyperclip.copy()
